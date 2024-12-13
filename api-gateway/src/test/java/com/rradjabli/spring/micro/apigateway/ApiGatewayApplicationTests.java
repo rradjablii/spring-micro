5c99f5f997dc7356650a0bc19c5773d82a3cd46a
rradjablii <ruslanradjabli.1@gmail.com>
2024-12-13 10:39:27 +0400

    Working Api Gateway

---
+++ api-gateway/src/test/java/com/rradjabli/spring/micro/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rradjabli.spring.micro.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
